d92845bf4f5e76220db435b1b4cc950d5e505ddc
Suleiman Moraes <30638390+Suleiman-Moraes@users.noreply.github.com>
2018-10-20 12:54:14 -0300

    Novos metodos no pesquisa js

--- src/main/webapp/resources/js/filtroregistro/pesquisa.js
+++ src/main/webapp/resources/js/filtroregistro/pesquisa.js
@@ -9,6 +9,14 @@ $(document).ready(
 		var nome = '*[id="formulario:listarRegistros:nome"]';
 		var descricao = '*[id="formulario:listarRegistros:descricao"]';
 		var id = '*[id="formulario:listarRegistros:id"]';
+		var precoPago = '*[id="formulario:listarRegistros:precoPago"]';
+		var porcentagemVenda = '*[id="formulario:listarRegistros:porcentagemVenda"]';
+		var quantidade = '*[id="formulario:listarRegistros:quantidade"]';
+		var valorAgrupamento = '*[id="formulario:listarRegistros:valorAgrupamento"]';
+		var marca = '*[id="formulario:listarRegistros:marca"]';
+		var valorMedida = '*[id="formulario:listarRegistros:valorMedida"]';
+		var descricao = '*[id="formulario:listarRegistros:descricao"]';
+		
 		var btnBuscar = '*[id="formulario:listarRegistros:btnBuscar"]';
 	
 		var globalFilter = '*[id="formulario:listarRegistros:globalFilter"]';
@@ -25,6 +33,48 @@ $(document).ready(
 		$(document).on('keypress', nome, function(e) {
 			if(e.keyCode == 13){
 				$(globalFilter).val($(nome).val());
+				PF('registroWV').filter();
+			}
+		});
+		
+		$(document).on('keypress', precoPago, function(e) {
+			if(e.keyCode == 13){
+				$(globalFilter).val($(precoPago).val());
+				PF('registroWV').filter();
+			}
+		});
+		
+		$(document).on('keypress', porcentagemVenda, function(e) {
+			if(e.keyCode == 13){
+				$(globalFilter).val($(porcentagemVenda).val());
+				PF('registroWV').filter();
+			}
+		});
+		
+		$(document).on('keypress', quantidade, function(e) {
+			if(e.keyCode == 13){
+				$(globalFilter).val($(quantidade).val());
+				PF('registroWV').filter();
+			}
+		});
+		
+		$(document).on('keypress', valorAgrupamento, function(e) {
+			if(e.keyCode == 13){
+				$(globalFilter).val($(valorAgrupamento).val());
+				PF('registroWV').filter();
+			}
+		});
+		
+		$(document).on('keypress', marca, function(e) {
+			if(e.keyCode == 13){
+				$(globalFilter).val($(marca).val());
+				PF('registroWV').filter();
+			}
+		});
+		
+		$(document).on('keypress', valorMedida, function(e) {
+			if(e.keyCode == 13){
+				$(globalFilter).val($(valorMedida).val());
 				PF('registroWV').filter();
 			}
 		});
