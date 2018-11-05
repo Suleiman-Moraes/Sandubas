e61e6219888423ce97d7c458968f002edfdcbf93
suleiman-am <suleiman-am@c-3786.agr.go>
2018-11-05 17:09:38 -0200

    teste com selenium

--- src/test/java/br/com/sandubas/service/TipoProdutoServiceTest.java
+++ src/test/java/br/com/sandubas/service/TipoProdutoServiceTest.java
@@ -23,64 +23,73 @@ import br.com.sandubas.exception.NegocioException;
 import br.com.sandubas.model.TipoProduto;
 import br.com.sandubas.util.jsf.FacesUtil;
 
-public class TipoProdutoServiceTest{
+public class TipoProdutoServiceTest {
 
 	private TipoProduto objeto = new TipoProduto("Teste Inserção", "Teste");
 	private static EntityManagerFactory factory;
 	private EntityManager manager;
-	
+
 	private static WebDriver driver;
-	
+
 	private TipoProdutoService service = new TipoProdutoService();
-	
+
 	@BeforeClass
 	public static void inicializaFabrica() {
-		System.setProperty("webdriver.chrome.driver", new File("src\\test\\resources\\br\\com\\sandubas\\driverselenium\\chromedriver.exe").getAbsolutePath());
+		System.setProperty("webdriver.chrome.driver",
+				new File("src\\test\\resources\\br\\com\\sandubas\\driverselenium\\chromedriver.exe")
+						.getAbsolutePath());
 
 		driver = new ChromeDriver();
 
 		driver.get("http://localhost:8080/sandubas/login.jsp");
 		driver.manage().window().maximize();
-		
-//		driver.findElement(By.className("close")).click();// Fechar modal
-		
+
+		// driver.findElement(By.className("close")).click();// Fechar modal
+
 		WebElement element = driver.findElement(By.id("login-username"));
 		element.sendKeys("root");
 		driver.findElement(By.id("login-password")).sendKeys("123456");
-		
+
 		driver.findElement(By.id("btn-login")).click();
-		
+
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 	}
-	
+
 	@Before
 	public void inicialiaDependencias() {
-//		System.out.println("inicialiaDependencias");
-//		this.manager = factory.createEntityManager();
-//		service.setPersistencia(new TipoProdutoDAO());
-//		service.getPersistencia().use("ProjetoPU");
-//		service.getPersistencia().setEntityManagerFactory(factory);
-//		service.getPersistencia().setEntityManager(manager);
+		// System.out.println("inicialiaDependencias");
+		// this.manager = factory.createEntityManager();
+		// service.setPersistencia(new TipoProdutoDAO());
+		// service.getPersistencia().use("ProjetoPU");
+		// service.getPersistencia().setEntityManagerFactory(factory);
+		// service.getPersistencia().setEntityManager(manager);
 	}
-	
-	@Test
-	public void testContarRegistrosCadastrados() {
-		driver.get("http://localhost:8080/sandubas/pages/mantertipoproduto/administrar.xhtml");
-		
-		WebElement element = driver.findElement(By.id("formulario:listarRegistros:totalRegistrosID"));
-		System.out.println(element.getText());
-		String label = element.getText().replaceAll("Total de Registros", "").trim();
-		System.out.println(label);
-		assertEquals(Integer.parseInt(label), 2);
-	}
+
+//	@Test
+//	public void testContarRegistrosCadastrados() {
+//		driver.get("http://localhost:8080/sandubas/pages/mantertipoproduto/administrar.xhtml");
+//
+//		WebElement element = driver.findElement(By.id("formulario:listarRegistros:totalRegistrosID"));
+//		System.out.println(element.getText());
+//		String label = element.getText().replaceAll("Total de Registros", "").trim();
+//		System.out.println(label);
+//		assertEquals(Integer.parseInt(label), 2);
+//	}
 
 	@Test
 	public void testSalvar() {
 		try {
+			driver.get("http://localhost:8080/sandubas/pages/mantertipoproduto/administrar.xhtml");
+			Thread.sleep(2000);
+			driver.findElement(By.id("formulario:j_idt45")).click();
+			Thread.sleep(2000);
+			driver.findElement(By.className("ui-panelgrid-cell")).sendKeys("Teste Selinium Inserção");
+			driver.findElement(By.id("formulario:descricao")).sendKeys("Teste Selinium Inserção");
+			driver.findElement(By.id("formulario:ButaoSalvar")).click();
 			String testeAlterar = "Teste alterar";
 			service.salvar(objeto);
 			assertTrue(objeto.getId() > 0);
@@ -93,16 +102,16 @@ public class TipoProdutoServiceTest{
 		}
 	}
 
-	@Test
-	public void testExcluir() {
-		try {
-			service.excluir(objeto);
-			Long id = service.getPersistencia().findById(TipoProduto.class, objeto.getId()).getId();
-			Long esperado = 0l;
-			assertEquals(null, id);
-		} catch (NegocioException e) {
-			assertTrue(Boolean.FALSE);
-		}
-	}
+//	@Test
+//	public void testExcluir() {
+//		try {
+//			service.excluir(objeto);
+//			Long id = service.getPersistencia().findById(TipoProduto.class, objeto.getId()).getId();
+//			Long esperado = 0l;
+//			assertEquals(null, id);
+//		} catch (NegocioException e) {
+//			assertTrue(Boolean.FALSE);
+//		}
+//	}
 
 }
